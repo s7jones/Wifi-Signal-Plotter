0782dbabe072e07c5cf9616b45ae8b52f6b509f8
Sam Jones <s7jones@users.noreply.github.com>
2016-05-07 00:41:59 +0100

    Added platform switches to allow first steps towards Windows support.
    Also ignores C0301 "line too long" pylint warning.

--- WifiSignalPlotter.py
+++ WifiSignalPlotter.py
@@ -7,6 +7,7 @@
 import subprocess
 import re
 import time
+import platform
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -27,12 +28,21 @@ while True:
 	dataArray = []
 
 	for a in range(0, 100):	#  x in [beginning, end)
-		p = subprocess.Popen("iwconfig", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
+		if platform.system() == 'Linux':
+			p = subprocess.Popen("iwconfig", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		elif platform.system() == 'Windows':
+			p = subprocess.Popen("netsh wlan show interfaces", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		else:
+			raise Exception('reached else of if statement')
 		out = p.stdout.read().decode()
 
-		m = re.findall('(wlan[0-9]+).*?Signal level=(-[0-9]+) dBm', out, re.DOTALL)
-
+		if platform.system() == 'Linux':
+			m = re.findall('(wlan[0-9]+).*?Signal level=(-[0-9]+) dBm', out, re.DOTALL)
+		elif platform.system() == 'Windows':
+			m = re.findall('Name.*?:.*?([A-z0-9 ]*).*?Signal.*?:.*?([0-9]*)%', out, re.DOTALL)
+		else:
+			raise Exception('reached else of if statement')
 
 		p.communicate()
 		elapsed = time.time() - t
