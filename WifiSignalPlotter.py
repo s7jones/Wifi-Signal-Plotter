635210da662fa2fd2fd406b1f584328aca3e32c3
Sam Jones <s7jones@users.noreply.github.com>
2016-05-07 00:16:04 +0100

    Amended to remove most of the pylint-ing errors (apart from "C0103, W0312").

--- WifiSignalPlotter.py
+++ WifiSignalPlotter.py
@@ -1,22 +1,24 @@
-## 	WifiSignalPlotter.py
-#		A quick script I've thrown together to get more familiar with Python
-#		and to check the difference in signal level between two Wifi adaptors
+# WifiSignalPlotter.py
+#	A quick script I've thrown together to get more familiar with Python
+#	and to check the difference in signal level between two Wifi adaptors
 
-import matplotlib.pyplot as plt
+""" Produces a plot of WiFi strength """
+
 import subprocess
 import re
 import time
+import matplotlib.pyplot as plt
 import numpy as np
 
 plt.ion
 
 fig = plt.figure()
-ax  = fig.add_subplot(111)
+ax = fig.add_subplot(111)
 
 t = time.time()
 
-avg = np.empty(shape=(2,0))
-std = np.empty(shape=(2,0))
+avg = np.empty(shape=(2, 0))
+std = np.empty(shape=(2, 0))
 
 times = np.empty(shape=(0))
 
@@ -24,25 +26,24 @@ while True:
 
 	dataArray = []
 
-	for a in range(0,100):	#  x in [beginning,end)
-		
+	for a in range(0, 100):	#  x in [beginning, end)
 		p = subprocess.Popen("iwconfig", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 		out = p.stdout.read().decode()
 
-		m = re.findall('(wlan[0-9]+).*?Signal level=(-[0-9]+) dBm',out,re.DOTALL)
+		m = re.findall('(wlan[0-9]+).*?Signal level=(-[0-9]+) dBm', out, re.DOTALL)
 
-		
-		p.communicate();
-		elapsed = time.time() - t;
 
-		dataArray.append(m);
+		p.communicate()
+		elapsed = time.time() - t
+
+		dataArray.append(m)
 
 
 		plt.pause(0.1)
 
-	counts = [0,0]
-	sortedData   = []
+	counts = [0, 0]
+	sortedData = []
 	sortedData.append([])
 	sortedData.append([])
 	for dataTuples in dataArray:
@@ -61,7 +62,7 @@ while True:
 	numArray = []
 	numArray.append([])
 	numArray.append([])
-	index = 0;
+	index = 0
 	for dataSet in sortedData:
 		for sdata in dataSet:
 			numArray[index].append(int(sdata))
@@ -72,9 +73,9 @@ while True:
 	lineSet = []
 	lineSet.append('b-')
 	lineSet.append('g-')
-	index = 0;
-	avgCurrent = np.zeros((2,1))
-	stdCurrent = np.zeros((2,1))
+	index = 0
+	avgCurrent = np.zeros((2, 1))
+	stdCurrent = np.zeros((2, 1))
 	for numSet in numArray:
 		avgCurrent[index] = np.mean(numSet)
 		stdCurrent[index] = np.std(numSet)
@@ -83,16 +84,16 @@ while True:
 	print(avgCurrent)
 	print(stdCurrent)
 
-	avg = np.append(avg,avgCurrent,axis=1)
-	std = np.append(std,stdCurrent,axis=1)
+	avg = np.append(avg, avgCurrent, axis=1)
+	std = np.append(std, stdCurrent, axis=1)
 
-	times = np.append(times,elapsed)
+	times = np.append(times, elapsed)
 
 	ax.clear()
 	plt.xlabel('Time [s]')
 	plt.ylabel('Signal Level [dBm]')
-	plt.errorbar(times[:],avg[0,:], yerr=std[0,:], label="wlan0 (usb)")
-	plt.errorbar(times[:],avg[1,:], yerr=std[1,:], label="wlan1 (internal)")
+	plt.errorbar(times[:], avg[0, :], yerr=std[0, :], label="wlan0 (usb)")
+	plt.errorbar(times[:], avg[1, :], yerr=std[1, :], label="wlan1 (internal)")
 	plt.legend()
 	print("\n\n")
 
